7f8d996b0fdda7c0c3752123c0da76034ee15365
Yadira Soto <lunar13heart@hotmail.com>
2019-06-26 17:45:35 -0400

    edits

--- liri.js
+++ liri.js
@@ -1,3 +1,34 @@
 // At the top of the liri.js file, add code to read and set any environment variables with the dotenv package:
 
 require("dotenv").config();
+var keys = require("./keys.js");
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
+var Spotify = require("node-spotify-api");
+
+var spotify = new spotify(keys.spotify);
+var omdbkey = keys.omdb.api_key;
+
+
+var whatToDo = process.argv[2];
+var userInput = process.argv[3];
+
+function spotifyThis() {
+    // go to npm spotify and copy paste the function and var info. Add limit if you want 1-10.
+
+    spotify
+        .search(
+            {
+                type: "track",
+                query: userInput,
+                limit: 5
+            }
+        )
+        .then(function ()
+}
+
+switch (whatToDo) {
+    case
+}
+
